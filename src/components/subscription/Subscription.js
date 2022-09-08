2a625e9c2fc104877bed4123f9e3c8edad225612
Hakar <hakar.yusuf@gmail.com>
2022-09-08 19:28:29 +0300

    test sub

--- src/components/subscription/Subscription.js
+++ src/components/subscription/Subscription.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import ReCAPTCHA from 'react-google-recaptcha';
+import React from 'react';
 import './SubscriptionStyles.scss';
 import Container from 'react-bootstrap/esm/Container';
 import Button from 'react-bootstrap/Button';
@@ -7,78 +6,7 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import { motion } from 'framer-motion';
 
-const Subscription = () => {
-  const [email, setEmail] = useState('');
-  const [submitting, setSubmitting] = useState(false);
-  const [note, setNote] = useState({
-    note: '',
-    status: '',
-  });
-
-  const recaptchaRef = React.createRef();
-
-  const subscribe = async (e) => {
-    e.preventDefault();
-    setSubmitting(true);
-    setNote({
-      note: '',
-      status: '',
-    });
-
-    const regex = new RegExp(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,7})+$/);
-    if (!regex.test(email)) {
-      setNote({
-        note: 'Email error. Please enter a valid email and try again.',
-        status: 'error',
-      });
-      return setSubmitting(false);
-    }
-
-    const token = await recaptchaRef.current.executeAsync();
-    if (!token) {
-      setNote({
-        note: 'Something went wrong. Please try again.',
-        status: 'error',
-      });
-      return setSubmitting(false);
-    }
-
-    try {
-      const data = JSON.stringify({ email, recaptcha_token: token });
-
-      await fetch(
-        'https://mailproxy.3air.io/api/sendgrid/v1/subscribe?tags=blog',
-        {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json, text/plain, */*',
-            'Content-type': 'application/json',
-          },
-          body: data,
-        }
-      )
-        .then(() => {
-          setNote({
-            note: 'Thank you for subscribing.',
-            status: 'success',
-          });
-          return setSubmitting(false);
-        })
-        .catch(() => {
-          setNote({
-            note: 'Something went wrong. Please try again.',
-            status: 'error',
-          });
-          return setSubmitting(false);
-        });
-    } catch (error) {
-      setNote({
-        note: 'Something went wrong. Please try again.',
-        status: 'error',
-      });
-      return setSubmitting(false);
-    }
-  };
+function Subscription() {
   return (
     <>
       <Container>
@@ -94,37 +22,20 @@ const Subscription = () => {
           }}
         >
           <h1>Register now to stay up-to-date!</h1>
-          <form id="newsletter" onSubmit={subscribe}>
-            <InputGroup>
-              {/* <ReCAPTCHA
-                ref={recaptchaRef}
-                size="invisible"
-                sitekey="6LcizmgfAAAAAAVBwAhBOTdchzmVJVkKR58dKp70"
-              /> */}
-              <Form.Control
-                placeholder="Email"
-                aria-label="Recipient's email"
-                aria-describedby="basic-addon2"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <Button
-                type="submit"
-                variant="outline-secondary"
-                id="button-addon2"
-                disabled={submitting}
-              >
-                SUBSCRIBE
-              </Button>
-              {/* <div id="subscribe-note" className={note.note ? note.status : ''}>
-                {note.note}
-              </div> */}
-            </InputGroup>
-          </form>
+          <InputGroup>
+            <Form.Control
+              placeholder="Email"
+              aria-label="Recipient's email"
+              aria-describedby="basic-addon2"
+            />
+            <Button variant="outline-secondary" id="button-addon2">
+              SUBSCRIBE
+            </Button>
+          </InputGroup>
         </motion.div>
       </Container>
     </>
   );
-};
+}
 
 export default Subscription;
