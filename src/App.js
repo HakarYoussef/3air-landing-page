dac72c9ef983e65bc594a07d120a0c8306d95732
Hakar <hakar.yusuf@gmail.com>
2022-09-15 21:48:45 +0300

    removed unused codes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import FrictionlessExperience from './components/FrictionlessExperience/FrictionlessExperience';
 import Header from './components/Header/Header';
