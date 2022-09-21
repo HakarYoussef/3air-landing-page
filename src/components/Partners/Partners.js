e1021d377326de3e697ff85164b3468309424829
waytothemoon <boikojin888@gmail.com>
2022-09-21 00:08:47 -0400

    Add Logos to PARTNERS & BACKERS

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -22,25 +22,46 @@ function Partners() {
         <Container className="logosContainer">
           <img height={36} src={process.env.PUBLIC_URL+'/assets/iceaddis.svg'} alt="Partners" />
 
-          <img height={22} src={process.env.PUBLIC_URL+'/assets/it.png'} alt="Partners" />
+          <img height={28} src={process.env.PUBLIC_URL+'/assets/it.png'} alt="Partners" />
 
-          <img height={36} src={process.env.PUBLIC_URL+'/assets/tele.png'} alt="Partners" />
+          <img height={36} src={process.env.PUBLIC_URL+'/assets/k3tele.png'} alt="Partners" />
 
-          <img height={58} src={process.env.PUBLIC_URL+'/assets/africa.png'} alt="Partners" />
+          <img height={84} src={process.env.PUBLIC_URL+'/assets/africa.png'} alt="Partners" />
 
-          <img height={38} src={process.env.PUBLIC_URL+'/assets/hero.png'} alt="Partners" />
+          <img height={36} src={process.env.PUBLIC_URL+'/assets/hero.png'} alt="Partners" />
 
-          <img height={32} src={process.env.PUBLIC_URL+'/assets/skale.svg'} alt="Partners" />
+          <img height={36} src={process.env.PUBLIC_URL+'/assets/skale.svg'} alt="Partners" />
 
-          <img height={32} src={process.env.PUBLIC_URL+'/assets/unilend.png'} alt="Partners" />
+          <img height={28} src={process.env.PUBLIC_URL+'/assets/okx.png'} alt="Partners" />
 
-          <img height={32} src={process.env.PUBLIC_URL+'/assets/bitliberte.png'} alt="Partners" />
+          <img height={28} src={process.env.PUBLIC_URL+'/assets/inacta.png'} alt="Partners" />
 
-          <img height={48} src={process.env.PUBLIC_URL+'/assets/knit.png'} alt="Partners" />
+          <img height={36} src={process.env.PUBLIC_URL+'/assets/balance.png'} alt="Partners" />
 
-          <img height={48} src={process.env.PUBLIC_URL+'/assets/ame.png'} alt="Partners" />
+          <img height={36} src={process.env.PUBLIC_URL+'/assets/nowp.png'} alt="Partners" />
 
-          <img height={48} src={process.env.PUBLIC_URL+'/assets/nest.png'} alt="Partners" />
+          <img height={36} src={process.env.PUBLIC_URL+'/assets/cafrica.png'} alt="Partners" />
+
+          <img height={36} src={process.env.PUBLIC_URL+'/assets/storm.png'} alt="Partners" />
+
+          <img height={36} src={process.env.PUBLIC_URL+'/assets/siban.png'} alt="Partners" />
+
+          <img height={36} src={process.env.PUBLIC_URL+'/assets/unilend.png'} alt="Partners" />
+
+          <img height={36} src={process.env.PUBLIC_URL+'/assets/pag.png'} alt="Partners" />
+
+          <img height={36} src={process.env.PUBLIC_URL+'/assets/solv.png'} alt="Partners" />
+
+          <img height={36} src={process.env.PUBLIC_URL+'/assets/synapse.png'} alt="Partners" />
+
+          <img height={36} src={process.env.PUBLIC_URL+'/assets/bitliberte.png'} alt="Partners" />
+
+          <img height={40} src={process.env.PUBLIC_URL+'/assets/knit.png'} alt="Partners" />
+
+          <img height={40} src={process.env.PUBLIC_URL+'/assets/ame.png'} alt="Partners" />
+
+          <img height={40} src={process.env.PUBLIC_URL+'/assets/nest.png'} alt="Partners" />
+        
         </Container>
       </Container>
     </>
