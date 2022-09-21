53f3d2b92b7a89bb5159e522139bd7e0929d2f9f
waytothemoon <boikojin888@gmail.com>
2022-09-20 21:57:02 -0400

    Countdown Fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Image from 'react-bootstrap/Image';
 import './Header.scss';
-import Countdown from '../Countdown/Countdown';
+import CountdownTimer from '../Countdown/CountdownTimer';
 
 export default function Header() {
   const [fix, setFix] = useState(false);
@@ -33,13 +33,64 @@ export default function Header() {
     setIsMenuClicked(!isMenuClicked);
   };
 
+  const unixEndDate = new Date(Date.UTC(2022, 8, 22, 10, 0, 0));
+  const initialCountdownTimer = {
+    days: '',
+    hours: '',
+    minutes: '',
+    seconds: ''
+  };
+
+  const [countdownSettings, setCountdownSettings] = useState(unixEndDate);
+  const [countdownTimer, setCountdownTimer] = useState({ ...initialCountdownTimer });
+
+  const playTimer = () => {
+    const now = new Date();
+
+    const distance = (countdownSettings - now) / 1000;
+
+    if (distance > 0) {
+      setCountdownTimer(prevCountdownTimer => {
+        return {
+          ...prevCountdownTimer,
+          days: parseInt(distance / (60 * 60 * 24), 10),
+          hours: parseInt(distance % (60 * 60 * 24) / (60 * 60), 10),
+          mins: parseInt(distance % (60 * 60) / (60), 10),
+          secs: parseInt(distance % 60, 10)
+        };
+      });
+    }
+    else {
+      setCountdownSettings( null );
+      setCountdownTimer({ ...initialCountdownTimer });
+    }
+  };
+
+  useEffect(() => {
+    let timer = null;
+    
+    if (unixEndDate) {
+      timer = setInterval(() => { 
+        playTimer(unixEndDate);}
+        , 1000);
+    }
+
+    return () => {
+      clearInterval(timer);
+      timer = null;
+    }
+  }, []);
+
   return (
     <Navbar
-      className={fix ? 'headerWrapperFixed' : 'headerWrapper'}
+      className={fix ? (unixEndDate > new Date() && countdownSettings ? 'headerWrapperFixed' : 'headerWrapperFixed headerWrapperMove')
+                      : (unixEndDate > new Date() && countdownSettings ? 'headerWrapper' : 'headerWrapper headerWrapperMove')}
       expand="lg"
     >
-      <Countdown className="countdown"/>
-      <Container className="header">
+      <div className={ unixEndDate > new Date() && countdownSettings ? "countdown" : "countdownHidden" }>
+        <CountdownTimer countdownTimer={countdownTimer} unixEndDate={countdownSettings} />
+      </div>
+      <Container className={unixEndDate > new Date() && countdownSettings ? "header" : "header headerMove"}>
         <Navbar.Brand href="#">
           {fix ? (
             <Image
@@ -60,7 +111,8 @@ export default function Header() {
         <div className="nav-items">
           {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
           <div
-            className={fix ? 'burger-menu-fixed' : 'burger-menu'}
+            className={fix ? (unixEndDate > new Date() &&countdownSettings ? 'burger-menu-fixed' : 'burger-menu-fixed burget-menu-move')
+                            : (unixEndDate > new Date() && countdownSettings ? 'burger-menu' : 'burger-menu burget-menu-move')}
             onClick={updateMenu}
           >
             <div className={burger_class}></div>
